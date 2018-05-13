918acc161b4f3abf14ee4b158ecba958efe51ad8
Joel George V <joel.gv3@gmail.com>
2018-05-13 19:52:11 +0530

    Cache variables, rename function

--- src/components/file-hash-container.js
+++ src/components/file-hash-container.js
@@ -15,7 +15,7 @@ export class FileHashContainer extends React.PureComponent {
   async componentDidMount() {
     try {
       const { hashType, arrayBuffer } = this.props;
-      const hash = await this.hash(hashType, arrayBuffer);
+      const hash = await this.calculateHash(hashType, arrayBuffer);
       this.setState({ hashType, hash });
     } catch (err) {
       console.error('Error during hash calculation: ', err);
@@ -23,15 +23,16 @@ export class FileHashContainer extends React.PureComponent {
   }
 
   static getDerivedStateFromProps(nextProps, prevState) {
-    return nextProps.hashType !== prevState.hashType ? { hashType: nextProps.hashType, hash: '' } : null
+    const { hashType } = nextProps;
+    return hashType !== prevState.hashType ? { hashType, hash: '' } : null
   }
 
   async componentDidUpdate(prevProps, prevState) {
-    const { arrayBuffer } = this.props;
-    const { hashType, hash } = this.state;
-    if (!hash) {
+    const { hashType } = this.state;
+    if (hashType !== prevState.hashType) {
       try {
-        const hash = await this.hash(hashType, arrayBuffer);
+        const { arrayBuffer } = this.props;
+        const hash = await this.calculateHash(hashType, arrayBuffer);
         this.setState({ hash });
       } catch (err) {
         console.error('Error during hash calculation: ', err);
@@ -39,7 +40,7 @@ export class FileHashContainer extends React.PureComponent {
     }
   }
 
-  hash = async (hashType, arrayBuffer) => {
+  calculateHash = async (hashType, arrayBuffer) => {
     const hashBuffer = await crypto.subtle.digest(hashType, arrayBuffer);
     const hashArray = Array.from(new Uint8Array(hashBuffer));
     const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
