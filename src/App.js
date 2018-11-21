5a4f36786842f2869c8a2dfca0578ca935334167
Joel George V <joel.gv3@gmail.com>
2018-11-21 22:51:45 +0000

    Complete redux, saga implementation

--- src/App.js
+++ src/App.js
@@ -1,65 +1,26 @@
 import React from 'react'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import createSagaMiddleware from 'redux-saga'
 import 'tachyons/css/tachyons.min.css'
 
-import { FilePicker, HashSelector, FileDetailsContainer } from './components'
+import { ShellContainer } from './containers'
+import { rootReducer } from './reducers'
+import { saga } from './sagas/saga'
 import github from './assets/github.svg'
 
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+sagaMiddleware.run(saga)
+
 export class App extends React.PureComponent {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      file: undefined,
-      hashType: '',
-      progress: 100,
-      loading: false
-    }
-  }
-
-  setFile = (e) => {
-    const files = e.dataTransfer && e.dataTransfer.files ? e.dataTransfer.files : e.target.files
-    if (files.length) {
-      this.setState({
-        file: files[0]
-      })
-    }
-    e.target.value = null
-  }
-
-  setHashType = (e) => {
-    if (this.state.file) {
-      this.setState({ loading: true })
-    }
-    this.setState({
-      hashType: e.target.value
-    })
-  }
-
-  setProgress = (progress) => {
-    if (progress === 100) {
-      this.setState({ loading: true })
-    }
-    if (progress === -1) {
-      this.setState({ file: undefined })
-    }
-    this.setState({ progress })
-  }
-
-  disableLoading = () => {
-    this.setState({ loading: false })
-  }
-
   render() {
-    const { file, hashType, progress, loading } = this.state
-    const disabled = (progress !== 100 && progress !== -1) || loading
     return (
       <div className='flex flex-column w-80 mw8 vh-100 center pv4'>
         <div className='flex flex-auto flex-column'>
-          <FilePicker onChange={this.setFile} disabled={disabled} />
-          <HashSelector hashType={hashType} onChange={this.setHashType} disabled={disabled} />
-          {file &&
-            <FileDetailsContainer file={file} hashType={hashType} progress={progress}
-              loading={loading} onFileRead={this.setProgress} onHashCompletion={this.disableLoading} />}
+          <Provider store={store}>
+            <ShellContainer />
+          </Provider>
         </div>
         <div className='self-center'>
           <a href='https://github.com/joelgeorgev/file-hash-verifier'>
--- src/components/file-loader.js
+++ src/components/file-loader.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 export class FileLoader extends React.PureComponent {
   render() {
-    const { progress, cancelLoad } = this.props
+    const { progress, cancelFileRead } = this.props
     return (
       <div className='flex justify-center mt4 b'>
         <div>
           <label>Loading file: {progress}%</label>
-          <button className='ml2' onClick={cancelLoad}>Cancel</button>
+          <button className='ml2' onClick={cancelFileRead}>Cancel</button>
         </div>
       </div>
     )
