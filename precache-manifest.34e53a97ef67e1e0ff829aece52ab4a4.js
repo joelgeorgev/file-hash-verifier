a271eb10591e363823b1b21a59d6e40005944c03
Joel George V <joel.gv3@gmail.com>
2019-01-30 00:40:53 +0000

    Updates

---
+++ precache-manifest.34e53a97ef67e1e0ff829aece52ab4a4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "362e17d51903148b9ca4",
+    "url": "/static/css/main.02612be8.chunk.css"
+  },
+  {
+    "revision": "362e17d51903148b9ca4",
+    "url": "/static/js/main.362e17d5.chunk.js"
+  },
+  {
+    "revision": "f9d70c623c2530667f6c",
+    "url": "/static/css/1.ccc4bb52.chunk.css"
+  },
+  {
+    "revision": "f9d70c623c2530667f6c",
+    "url": "/static/js/1.f9d70c62.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1e918ea2260cf4f838495e69fab1e878",
+    "url": "/static/media/clippy.1e918ea2.svg"
+  },
+  {
+    "revision": "4f20b95a481566084c191047ff7949ae",
+    "url": "/static/media/success.4f20b95a.svg"
+  },
+  {
+    "revision": "1e98e874e408dbf5402cee3829427203",
+    "url": "/static/media/fail.1e98e874.svg"
+  },
+  {
+    "revision": "dc6635a4b3f40c69ee44a80625b8fcdc",
+    "url": "/static/media/github.dc6635a4.svg"
+  },
+  {
+    "revision": "76d8cbb0496cb184eff868152b67ad45",
+    "url": "/static/media/source-sans-pro-v11-latin-regular.76d8cbb0.woff2"
+  },
+  {
+    "revision": "f94f84eb867a3e3d65da0c5cad065437",
+    "url": "/static/media/source-sans-pro-v11-latin-regular.f94f84eb.woff"
+  },
+  {
+    "revision": "8986ac224e429c331fe255f4db499e48",
+    "url": "/index.html"
+  }
+];
