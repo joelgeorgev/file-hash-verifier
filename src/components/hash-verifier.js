92f584107fca83971c8a766101456f0dc42b3cc3
Joel George V <joel.gv3@gmail.com>
2019-03-23 19:30:16 +0000

    Use state hook

--- src/components/hash-verifier.js
+++ src/components/hash-verifier.js
@@ -1,59 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import success from '../assets/success.svg'
 import fail from '../assets/fail.svg'
 
-export class HashVerifier extends React.PureComponent {
+export const HashVerifier = ({ hash }) => {
+  const [verify, setVerify] = useState(false)
+  const [text, setText] = useState('')
+  const [match, setMatch] = useState(false)
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      verify: false,
-      text: '',
-      match: false
-    }
+  const toggleVerify = (e) => setVerify(e.target.checked)
+
+  const verifyHash = (e) => {
+    const inputText = e.target.value
+    setText(inputText)
+    setMatch(hash === inputText.replace(/ /g, '').toLowerCase())
   }
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    return {
-      match: nextProps.hash === prevState.text.replace(/ /g, '').toLowerCase()
-    }
-  }
-
-  toggleVerify = (e) => {
-    this.setState({
-      verify: e.target.checked
-    })
-  }
-
-  verifyHash = (e) => {
-    const text = e.target.value
-    this.setState({
-      text,
-      match: this.props.hash === text.replace(/ /g, '').toLowerCase()
-    })
-  }
-
-  render() {
-    const { verify, text, match } = this.state
-    return (
-      <div className='flex flex-column mt4'>
-        <div>
-          <input type='checkbox' checked={verify} className='pointer'
-            onChange={this.toggleVerify} />
-          <label className='ml2 b'>Compare with:</label>
-        </div>
-        {verify &&
-          <div className='flex h2'>
-            <input type='text' value={text}
-              className='w-100 ph1 bt br-0 bb bl b--light-silver'
-              onChange={this.verifyHash} />
-            <button className='bg-transparent ba b--light-silver'>
-              <img src={match ? success : fail}
-                alt={match ? 'Success' : 'Fail'} className='w1' />
-            </button>
-          </div>}
+  return (
+    <div className='flex flex-column mt4'>
+      <div>
+        <input type='checkbox' checked={verify} className='pointer'
+          onChange={toggleVerify} />
+        <label className='ml2 b'>Compare with:</label>
       </div>
-    )
-  }
+      {verify &&
+        <div className='flex h2'>
+          <input type='text' value={text}
+            className='w-100 ph1 bt br-0 bb bl b--light-silver'
+            onChange={verifyHash} />
+          <button className='bg-transparent ba b--light-silver'>
+            <img src={match ? success : fail}
+              alt={match ? 'Success' : 'Fail'} className='w1' />
+          </button>
+        </div>}
+    </div>
+  )
 }
