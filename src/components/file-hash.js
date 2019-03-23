3f0d56de77976b1660c9c0114f552db002f9a424
Joel George V <joel.gv3@gmail.com>
2019-03-23 19:07:45 +0000

    Use ref hook

--- src/components/file-hash.js
+++ src/components/file-hash.js
@@ -1,11 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import Clipboard from 'clipboard'
 
 import clippy from '../assets/clippy.svg'
 
 export const FileHash = ({ hash }) => {
+  const ref = useRef(null)
+
   useEffect(() => {
-    const clipboard = new Clipboard('#btn')
+    const { current: element } = ref
+    const clipboard = new Clipboard(element)
     return () => clipboard.destroy()
   }, [])
 
@@ -15,7 +18,7 @@ export const FileHash = ({ hash }) => {
       <div className='flex h2'>
         <input type='text' id='hash' value={hash} readOnly
           className='w-100 ph1 bt br-0 bb bl b--light-silver' />
-        <button id='btn' data-clipboard-target='#hash'
+        <button ref={ref} data-clipboard-target='#hash'
           className='bg-transparent ba b--light-silver pointer'>
           <img src={clippy} alt='Copy to clipboard' className='w1' />
         </button>
