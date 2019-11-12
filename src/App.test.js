494c8799b0833e9fd353ed5aaa6cba9e00232ae2
Joel George V <joel.gv3@gmail.com>
2019-11-12 22:55:40 +0000

    Update smoke test

--- src/App.test.js
+++ src/App.test.js
@@ -1,24 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import renderer from 'react-test-renderer'
+import { render, unmountComponentAtNode } from 'react-dom'
 
 import { App } from './App'
 
-it('smoke test', () => {
-  const container = document.createElement('div')
-  ReactDOM.render(<App />, container)
-})
-
-it('radio button state change test', () => {
-  const container = document.createElement('div')
-  renderer.act(() => {
-    ReactDOM.render(<App />, container)
+describe('App', () => {
+  test('renders', () => {
+    const container = document.createElement('div')
+    render(<App />, container)
+    unmountComponentAtNode(container)
+    container.remove()
   })
-
-  // Get 'SHA-256' radio button node
-  const inputElement = container.querySelectorAll('input')[2]
-  renderer.act(() => {
-    inputElement.dispatchEvent(new MouseEvent('click', { bubbles: true }))
-  })
-  expect(inputElement.checked).toBe(true)
 })
