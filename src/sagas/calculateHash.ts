a5cbeaf7737ea7c465076a547caad5bb2e71558d
Joel George V <joel.gv3@gmail.com>
2021-04-28 20:14:12 +0100

    Update calculate hash saga - Use a single state selector - Clean up types - Move null checks outside hash calculation function

--- src/sagas/calculateHash.ts
+++ src/sagas/calculateHash.ts
@@ -2,34 +2,28 @@ import { put, call, select } from 'redux-saga/effects'
 
 import { hashCalculated } from '../actions'
 import type { State } from '../store'
-import type { Hash } from '../types'
+import type { HashType, Hash } from '../types'
 
-type ArrayBuffer = State['arrayBuffer']
-type HashType = State['hashType']
-
-const getHash = async (
+const getFileHash = async (
   arrayBuffer: ArrayBuffer,
   hashType: HashType
 ): Promise<Hash> => {
-  if (arrayBuffer && hashType) {
-    const hashBuffer = await crypto.subtle.digest(hashType, arrayBuffer)
-    const hashArray = Array.from(new Uint8Array(hashBuffer))
-    const hashHex = hashArray
-      .map((b) => ('00' + b.toString(16)).slice(-2))
-      .join('')
-    return hashHex
-  }
-  return ''
+  const hashBuffer = await crypto.subtle.digest(hashType, arrayBuffer)
+  const hashArray = Array.from(new Uint8Array(hashBuffer))
+  const hashHex = hashArray
+    .map((b) => ('00' + b.toString(16)).slice(-2))
+    .join('')
+
+  return hashHex
 }
 
-const getArrayBuffer = (state: State): ArrayBuffer => state.arrayBuffer
-const getHashType = (state: State): HashType => state.hashType
-
 export const calculateHash = function* () {
-  const arrayBuffer: ArrayBuffer = yield select(getArrayBuffer)
-  const hashType: HashType = yield select(getHashType)
+  const { arrayBuffer, hashType }: State = yield select()
 
-  const hash: Hash = yield call(getHash, arrayBuffer, hashType)
+  if (!arrayBuffer || !hashType) {
+    return
+  }
 
+  const hash: Hash = yield call(getFileHash, arrayBuffer, hashType)
   yield put(hashCalculated(hash))
 }
