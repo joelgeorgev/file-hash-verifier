cc4c34a405d92abdd4e52f00fceed00f60f6c6e6
Joel George V <joel.gv3@gmail.com>
2021-04-29 13:45:48 +0100

    Infer return type of hash function

--- src/sagas/calculateHash.ts
+++ src/sagas/calculateHash.ts
@@ -1,13 +1,13 @@
-import { put, call, select } from 'redux-saga/effects'
+import { put, call, select, SagaReturnType } from 'redux-saga/effects'
 
 import { hashCalculated } from '../actions'
 import type { State } from '../store'
-import type { HashType, Hash } from '../types'
+import type { HashType } from '../types'
 
 const getFileHash = async (
   arrayBuffer: ArrayBuffer,
   hashType: HashType
-): Promise<Hash> => {
+): Promise<string> => {
   const hashBuffer = await crypto.subtle.digest(hashType, arrayBuffer)
   const hashArray = Array.from(new Uint8Array(hashBuffer))
   const hashHex = hashArray
@@ -24,6 +24,10 @@ export const calculateHash = function* () {
     return
   }
 
-  const hash: Hash = yield call(getFileHash, arrayBuffer, hashType)
+  const hash: SagaReturnType<typeof getFileHash> = yield call(
+    getFileHash,
+    arrayBuffer,
+    hashType
+  )
   yield put(hashCalculated(hash))
 }
